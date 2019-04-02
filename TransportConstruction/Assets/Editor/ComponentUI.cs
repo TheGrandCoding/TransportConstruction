39bbf6a5b20410eda4d7198dff2941edebfc782b
CheAle14 <thedark1513@gmail.com>
2019-04-02 21:43:21 +0100

    attempts

---
+++ TransportConstruction/Assets/Editor/ComponentUI.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+/*//[CustomEditor(typeof(Component))]
+public class ComponentUI : Editor
+{
+    static bool showRequiredObjects = false;
+    public override void OnInspectorGUI()
+    {
+        //DrawDefaultInspector();
+        var script = (Component)target;
+        //Debug.Log("Doing for: " + script.Name);
+        //GUILayout.TextField("Name", script.Name);
+        showRequiredObjects = EditorGUILayout.Foldout(showRequiredObjects, "Needed components");
+        if(showRequiredObjects)
+        {
+            Editor editor = null;
+            foreach(var component in script.RequiredComponents)
+            {
+                EditorGUI.indentLevel++;
+                if (!editor)
+                    Editor.CreateCachedEditor(component, typeof(RequireComponent), ref editor);
+                editor.OnInspectorGUI();
+                EditorGUI.indentLevel--;
+            }
+        }
+
+
+        if (GUILayout.Button("New"))
+        {
+            Debug.Log("Thing for " + script.Name);
+            script.AddNewRequirement();
+        }
+    }
+}*/
