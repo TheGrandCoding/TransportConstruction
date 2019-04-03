8d366bae987534dcc597097e34ceb70441492f1b
CheAle14 <thedark1513@gmail.com>
2019-04-03 15:52:43 +0100

    Done what tom did, only better

--- TransportConstruction/Assets/Editor/ComponentUI.cs
+++ TransportConstruction/Assets/Editor/ComponentUI.cs
@@ -4,35 +4,25 @@ using System.Collections.Generic;
 using UnityEditor;
 using UnityEngine;
 
-/*//[CustomEditor(typeof(Component))]
+[CustomEditor(typeof(Component))]
 public class ComponentUI : Editor
 {
     static bool showRequiredObjects = false;
     public override void OnInspectorGUI()
     {
-        //DrawDefaultInspector();
+        DrawDefaultInspector();
         var script = (Component)target;
         //Debug.Log("Doing for: " + script.Name);
         //GUILayout.TextField("Name", script.Name);
-        showRequiredObjects = EditorGUILayout.Foldout(showRequiredObjects, "Needed components");
-        if(showRequiredObjects)
+        if (GUILayout.Button("Log Base Components Needed"))
         {
-            Editor editor = null;
-            foreach(var component in script.RequiredComponents)
+            string log = "";
+            var dict = script.GetTotalBaseComponents();
+            foreach(var keypair in dict)
             {
-                EditorGUI.indentLevel++;
-                if (!editor)
-                    Editor.CreateCachedEditor(component, typeof(RequireComponent), ref editor);
-                editor.OnInspectorGUI();
-                EditorGUI.indentLevel--;
+                log += $"{keypair.Key.Name} : {keypair.Value}\r\n";
             }
-        }
-
-
-        if (GUILayout.Button("New"))
-        {
-            Debug.Log("Thing for " + script.Name);
-            script.AddNewRequirement();
+            Debug.LogWarning($"For {script.Name}:\r\n{log}");
         }
     }
-}*/
+}
