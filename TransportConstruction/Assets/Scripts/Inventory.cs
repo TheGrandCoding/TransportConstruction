f0b675e77c7449467d603d0b180e1335ad4a598b
CheAle14 <thedark1513@gmail.com>
2019-04-05 19:17:45 +0100

    idk

---
+++ TransportConstruction/Assets/Scripts/Inventory.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Inventory : MonoBehaviour
+{
+    static Inventory instance;
+    private void Awake()
+    {
+        instance = this;
+    }
+
+    public static Dictionary<Component, int> Components = new Dictionary<Component, int>();
+
+    static int _remove(Component c, int maximum = int.MaxValue)
+    {
+        int numRemoved = 0;
+        if(Components.TryGetValue(c, out int numOfThisComponent))
+        {
+            while(numOfThisComponent > 0 && numRemoved < maximum)
+            {
+                numRemoved++;
+                numOfThisComponent--;
+            }
+            Components[c] = numOfThisComponent;
+        }
+        return numRemoved;
+    }
+
+    static void _add(Component c)
+    {
+        if(Components.TryGetValue(c, out int amount))
+        {
+            Components[c] = amount + 1;
+        } else
+        {
+            Components.Add(c, 1);
+        }
+    }
+
+
+    public int Remove(Component c, int maximum = int.MaxValue) => _remove(c, maximum);
+
+    public void Add(Component c) => _add(c);
+}
---
+++ TransportConstruction/Assets/Scripts/Machine.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Machine : MonoBehaviour
+{
+    public string Name;
+
+    public bool CanProduce(Component[] itemsIn, Component wantedOut)
+    {
+        if(wantedOut.Machine == Name)
+        {
+            List<Component> needed = wantedOut.Components;
+            foreach (var c in itemsIn)
+                needed.Remove(c);
+            return needed.Count == 0;
+        }
+        return false;
+    }
+
+
+    public bool Produce(Inventory inventory, Component wantedComponent, Component[] itemsIn)
+    {
+        if(CanProduce(itemsIn, wantedComponent))
+        {
+            foreach(var c in itemsIn)
+            {
+                inventory.Remove(c, 1);
+            }
+            inventory.Add(wantedComponent);
+            return true;
+        }
+        return false;
+    }
+}
