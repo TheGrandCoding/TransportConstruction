a8d5b6d5aa7ae718cd1a4005b4bb3ac379be21fe
LAPTOP-GV137KLR\tomal <tomalfieb10@gmail.com>
2019-04-08 20:34:43 +0100

    Added conveyor belts that work
    the script will autofill parameters as long as the object has a child called "Endpoint"

---
+++ TransportConstruction/Assets/Scripts/ConveryorBelt.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ConveryorBelt : MonoBehaviour
+{
+    public GameObject belt;
+    public Transform endpoint;
+    public float speed = 1f;
+
+    private void Start()
+    {
+        belt = this.gameObject;
+        endpoint = transform.Find("Endpoint");
+    }
+
+    private void OnTriggerStay(Collider other)
+    {
+        other.transform.position = Vector3.MoveTowards(other.transform.position, endpoint.position, speed * Time.deltaTime);
+    }
+}
