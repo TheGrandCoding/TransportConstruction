760a6ae00afe5f0613db7d4b8efc109989c4a4bd
LAPTOP-GV137KLR\tomal <tomalfieb10@gmail.com>
2019-04-08 21:09:30 +0100

    Added script to spawn construction arm bases

---
+++ TransportConstruction/Assets/Scripts/ConstructionArmSpawner.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ConstructionArmSpawner : MonoBehaviour
+{
+    public GameObject armBase;
+    public Vector3 scale = new Vector3(1.5f, 1.5f, 1);
+
+    void Start()
+    {
+        GameObject newArmBase = Instantiate(armBase, transform.position, armBase.transform.rotation);
+        newArmBase.transform.localScale = scale;
+        newArmBase.transform.parent = gameObject.transform;
+    }
+}
